c2e07248efc393686ee348d48bd372c9b1373a68
Softdevelop <softdevelop.inc@gmail.com>
2014-03-20 16:36:01 +0700

    Edit by Hoang

--- slide/js/tour.js
+++ slide/js/tour.js
@@ -1,14 +1,14 @@
 $(document).ready(function (){
 	var html = $('.formItem.clone').clone();
 	var add ='<form class="formItem"><input type="button" class="btn_add"/><input type="text" name=""/><input type="text" name=""/><input type="text" name=""/><input type="button" class="btn_close"/></form>';
-	$('.addItem').click(function(){
-	$('.item').append(add) });
-	$('.btn_close').click(function(){
-	
+	$('.addItem').on('click', function(){
+		$('.item').append(add);
+	});
+	$('.item').on('click', '.formItem .btn_close', function(){
 		$(this).parent().remove();
 	});
-	var add1 = '<form class="formproject"><input type="button"  class="cross btn_close"/><input type="button" class="minus"/><input type="button"  class="plus"/><input type="text" class="project"/><input type="text" class="reduction"/><input type="text" class="weather"/><input type="text" class="skin"/><input type="button"  class="order"/><input type="button" class="image"/></form>';
-	$('.btn_projectitem').click(function(){
+	var add1 = '<form class="formproject"><input type="button"  class="cross btn_close"/><input type="button" class="minus"/><input type="button"  class="plus"/><input type="text" class="project"/><input type="text" class="reduction"/><input type="text" class="weather"/><input type="text" class="skin"/><input type="button"  class="order"/><img scr="" class="image" /></form>';
+	$('.btn_projectitem').on('click', function(){
 		$('.form').append(add1);
 	});
 });
